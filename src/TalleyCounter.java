c202e12332723c9b0d4efd60aea33fc4d827bbb0
Daniel <42978531+danielhan1@users.noreply.github.com>
2018-09-26 14:28:14 -0400

    Add files via upload

--- src/TalleyCounter.java
+++ src/TalleyCounter.java
@@ -1,4 +1,33 @@
-
-public class TalleyCounter {
-
+/**
+ * 
+ * 
+ * 
+ * @author DanielHan
+ *
+ */
+public class TalleyCounter
+{
+	// Instance Variables
+	
+	private int count;
+	
+	
+	// Constructor
+	
+	public TalleyCounter()
+	{
+		count = 0;
+	}
+	
+	public TalleyCounter(int startNum)
+	{
+		count = startNum;
+		
+	}
+	
+	public void click()
+	{
+		count = count + 1;
+		//count++;
+	}
 }
